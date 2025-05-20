16459286f65c7620988903ce00b40490b46fbd56
Pedro Mota <110862249+Psm0015@users.noreply.github.com>
2025-05-20 08:18:27 -0300

    Implementação Swagger

--- src/main/java/io/github/psm0015/libraryapi/config/SecurityConfiguration.java
+++ src/main/java/io/github/psm0015/libraryapi/config/SecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -36,6 +37,21 @@ public class SecurityConfiguration {
                 })
                 .build();
     }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        return  web -> {
+            web.ignoring().requestMatchers(
+                    "/v2/api-docs/**",
+                    "/v3/api-docs/**",
+                    "/swagger-resources/**",
+                    "/swagger-ui.html",
+                    "/swagger-ui/**",
+                    "/webjars/**"
+            );
+        };
+    }
+
 
     @Bean
     public PasswordEncoder passwordEncoder(){
